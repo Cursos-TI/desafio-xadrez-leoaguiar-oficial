e7de6ad228cc76c56aebd8d3ee96bd9a74685f02
Leonardo <leo.aguiarbr@gmail.com>
2025-05-03 21:31:09 +0000

    Atualização e tentativa de colocar sistema de_ escolhas, mas verei uma forma melhor de fazer_ sem deixar o código tão longo.

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,8 @@
 
 int main(){
 
-    int jogador, bispo = 0, torre, rainha; // Variáveis 
+    int jogador, bispo = 0, torre, rainha, casas; // Variáveis
+    int paraCima, paraBaixo, direita, esquerda; // Variáveis de direção 
     /*
     Torre - For
     Bispo - While
@@ -36,6 +37,99 @@ int main(){
     printf("Escolha: ");
     scanf("%d", &jogador);
 
+    // ====== Escolha da Direção ======
+    printf("\n||| Escolha a direção |||\n");
+    printf("1. Para cima.\n");
+    printf("2. Para baixo.\n");
+    printf("3. Para esquerda.\n");
+    printf("4. Para direita.\n");
+    printf("Escolha: ");
+    scanf("%d", &jogador);
+
+    switch (jogador)
+    if (jogador == torre)
+    {
+    
+    {
+    case 1:
+        if (jogador == 1)
+        {
+            printf("Mover quantas vezes?\n");
+            printf("1.\n");
+            printf("2.\n");
+            printf("3.\n");
+            printf("4.\n");
+            printf("5.\n");
+            printf("6.\n");
+            printf("7.\n");
+            printf("8.\n");
+            printf("Selecione: ");
+            scanf("%d", &casas);
+        } for (torre = 0; torre < casas; torre++) // Valor inicial da Torre (0) e até quantas casas andará (5)
+        {
+            printf("Para cima %d vezes!\n", casas);
+        }
+        break;    
+    
+    case 2:
+    if (jogador == 2)
+    {
+        printf("Mover quantas vezes?\n");
+        printf("1.\n");
+        printf("2.\n");
+        printf("3.\n");
+        printf("4.\n");
+        printf("5.\n");
+        printf("6.\n");
+        printf("7.\n");
+        printf("8.\n");
+        printf("Selecione: ");
+        scanf("%d", &casas);
+    }
+
+        break;
+
+    case 3:
+    if (jogador == 3)
+    {
+        printf("Mover quantas vezes?\n");
+        printf("1.\n");
+        printf("2.\n");
+        printf("3.\n");
+        printf("4.\n");
+        printf("5.\n");
+        printf("6.\n");
+        printf("7.\n");
+        printf("8.\n");
+        printf("Selecione: ");
+        scanf("%d", &casas);
+    }
+
+        break;
+
+    case 4:
+    if (jogador == 4)
+        {
+            printf("Mover quantas vezes?\n");
+            printf("1.\n");
+            printf("2.\n");
+            printf("3.\n");
+            printf("4.\n");
+            printf("5.\n");
+            printf("6.\n");
+            printf("7.\n");
+            printf("8.\n");
+            printf("Selecione: ");
+            scanf("%d", &casas);
+        }
+
+        break;
+    
+    default:
+        break;
+    }
+}
+    /*
     // ----- Torre -----
     if (jogador == 1){
     for (torre = 0; torre <5 ; torre++) // Valor inicial da Torre (0) e até quantas casas andará (5)
@@ -71,6 +165,7 @@ int main(){
     
             printf("jogo encerrado.");
     }
+            */
     
     return 0;
 }
